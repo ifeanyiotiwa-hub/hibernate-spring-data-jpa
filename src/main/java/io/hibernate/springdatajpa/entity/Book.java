8ba65a9b0a1db4f531cac83cec8588894ec3b919
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-24 18:50:27 +0100

    creates Book entity

---
+++ src/main/java/io/hibernate/springdatajpa/entity/Book.java
@@ -0,0 +1,83 @@
+package io.hibernate.springdatajpa.entity;
+
+
+import com.google.common.base.MoreObjects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String title;
+    private String isbn;
+    private String author;
+    private String publisher;
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Book(String title, String isbn, String author, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+        this.publisher = publisher;
+    }
+    
+    public Book() {
+    
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public String getPublisher() {
+        return publisher;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("title", title)
+                .add("isbn", isbn)
+                .add("author", author)
+                .add("publisher", publisher)
+                .toString();
+    }
+}
