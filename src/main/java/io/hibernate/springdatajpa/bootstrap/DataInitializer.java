7a0655ba1783dafd23913af02e5fa84dfcc0a00d
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-25 16:15:41 +0100

    updates DataInitializer.java

--- src/main/java/io/hibernate/springdatajpa/bootstrap/DataInitializer.java
+++ src/main/java/io/hibernate/springdatajpa/bootstrap/DataInitializer.java
@@ -6,9 +6,11 @@ import static io.hibernate.springdatajpa.entity.Book.BookBuilder;
 import io.hibernate.springdatajpa.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 
+@Profile({"local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
     private final BookRepository bookRepository;
@@ -19,6 +21,7 @@ public class DataInitializer implements CommandLineRunner {
     
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
         
         Book bookDDD = new BookBuilder().title("Domain Driven Design 2014")
                                .author("Rando Author")
