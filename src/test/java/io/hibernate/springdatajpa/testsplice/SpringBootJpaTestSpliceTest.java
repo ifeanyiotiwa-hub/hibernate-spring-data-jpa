2cf4a995f102dd3177416f7a4bde34b5f1b6ff15
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-25 16:16:59 +0100

    modifies SpringBootJpaTestSpliceTest class

--- src/test/java/io/hibernate/springdatajpa/testsplice/SpringBootJpaTestSpliceTest.java
+++ src/test/java/io/hibernate/springdatajpa/testsplice/SpringBootJpaTestSpliceTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -16,6 +18,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
+@ComponentScan(basePackages = "io.hibernate.springdatajpa.bootstrap")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SpringBootJpaTestSpliceTest {
     
     @Autowired
@@ -26,7 +30,7 @@ public class SpringBootJpaTestSpliceTest {
     @Test
     protected void testJpaSlice() {
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(0);
+        assertThat(countBefore).isEqualTo(2);
     
         Book newBook = new Book.BookBuilder().title("Test1")
                                .author("TestAuthor1")
@@ -42,6 +46,6 @@ public class SpringBootJpaTestSpliceTest {
     @Test
     public void testJpaSpliceTransaction() {
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(1);
+        assertThat(countBefore).isEqualTo(3);
     }
 }
