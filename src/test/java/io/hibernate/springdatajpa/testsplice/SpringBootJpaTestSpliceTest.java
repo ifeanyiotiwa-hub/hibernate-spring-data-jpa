65b9a9ce06ad8a5d977a3c227d530e9136199c9d
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-25 09:36:26 +0100

    creates Test class

---
+++ src/test/java/io/hibernate/springdatajpa/testsplice/SpringBootJpaTestSpliceTest.java
@@ -0,0 +1,47 @@
+package io.hibernate.springdatajpa.testsplice;
+
+
+import io.hibernate.springdatajpa.entity.Book;
+import io.hibernate.springdatajpa.repository.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DataJpaTest
+public class SpringBootJpaTestSpliceTest {
+    
+    @Autowired
+    private BookRepository bookRepository;
+    
+    @Commit
+    @Order(1)
+    @Test
+    protected void testJpaSlice() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(0);
+    
+        Book newBook = new Book.BookBuilder().title("Test1")
+                               .author("TestAuthor1")
+                               .isbn("Test1234")
+                               .publisher("TestPublisher1")
+                               .build();
+        bookRepository.save(newBook);
+        long countAfter = bookRepository.count();
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+    
+    @Order(2)
+    @Test
+    public void testJpaSpliceTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(1);
+    }
+}
