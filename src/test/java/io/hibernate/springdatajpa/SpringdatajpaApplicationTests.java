6fbcd777953336b7bb8f7d8cb8c458f9921b8830
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-25 07:42:38 +0100

    wrote a single test

--- src/test/java/io/hibernate/springdatajpa/SpringdatajpaApplicationTests.java
+++ src/test/java/io/hibernate/springdatajpa/SpringdatajpaApplicationTests.java
@@ -1,10 +1,25 @@
 package io.hibernate.springdatajpa;
 
+import io.hibernate.springdatajpa.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SpringdatajpaApplicationTests {
+    
+    @Autowired
+    BookRepository testRepository;
+    
+    
+    @Test
+    void testBookRepository() {
+        long count = testRepository.count();
+        
+        assertThat(count).isGreaterThan(0);
+    }
     
     @Test
     void contextLoads() {
