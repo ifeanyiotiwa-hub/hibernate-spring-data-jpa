79062ab935ab660a1418aeb429606d2bc2ef5754
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-26 23:11:16 +0100

    adds two test methods for MySQLIntegrationTest

--- src/test/java/io/hibernate/springdatajpa/integrationtest/MySQLIntegrationTest.java
+++ src/test/java/io/hibernate/springdatajpa/integrationtest/MySQLIntegrationTest.java
@@ -1,12 +1,18 @@
 package io.hibernate.springdatajpa.integrationtest;
 
+import io.hibernate.springdatajpa.entity.AuthorUuid;
+import io.hibernate.springdatajpa.entity.BookUuid;
+import io.hibernate.springdatajpa.repository.AuthorUuidRepository;
 import io.hibernate.springdatajpa.repository.BookRepository;
+import io.hibernate.springdatajpa.repository.BookUuidRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,9 +24,34 @@ public class MySQLIntegrationTest {
     @Autowired
     BookRepository bookRepository;
     
+    @Autowired
+    BookUuidRepository bookUuidRepository;
+    
+    @Autowired
+    AuthorUuidRepository authorUuidRepository;
+    
     @Test
     public void testMySQL() {
         long count = bookRepository.count();
         assertThat(count).isEqualTo(2);
+    }
+    
+    @Test
+    public void testAuthorUuid() {
+        long count = authorUuidRepository.count();
+        AuthorUuid authorUuid = authorUuidRepository.save(new AuthorUuid("Chika", "Nwobi"));
+        long finalCount = authorUuidRepository.count();
+        assertThat(authorUuid).isNotNull();
+        assertThat(count).isLessThan(finalCount);
+        
+        AuthorUuid fetched = authorUuidRepository.getById(authorUuid.getId());
+        assertThat(fetched).isNotNull();
+        assertThat(fetched).isEqualTo(authorUuid);
+    }
+    
+    @Test
+    public void testSavedBookUuid() {
+        BookUuid bookUuid = bookUuidRepository.save(new BookUuid.BookBuilder().title("Test title").build());
+        assertThat(bookUuid.getId()).isNotNull();
     }
 }
